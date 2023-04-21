c7be2174e5d0fdf21a1cdffdf3c192a98d9418ef
ErickMwangi <rickthetri20@gmail.com>
2023-04-21 13:22:39 +0300

    completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-function ColorList() {
+
+function ColorList(props) {
   const colors = [
     "firebrick",
     "rebeccapurple",
@@ -30,7 +31,11 @@ function ColorList() {
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        {/*display the array of <li> elements here! */}
+          <li style={{ color: colors[0] }}>{colors[0]}</li>
+          <li style={{ color: colors[1] }}>{colors[1]}</li>
+          <li style={{ color: colors[2] }}>{colors[2]}</li>
+          <li style={{ color: colors[3] }}>{colors[3]}</li>
+          <li style={{ color: colors[4] }}>{colors[4]}</li>
       </ol>
     </div>
   );
